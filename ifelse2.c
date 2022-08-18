9189c633daa04e93553587484000a2ee958668db
sudarshansp <111242373+sudarshansp@users.noreply.github.com>
2022-08-18 23:01:43 +0530

    Create ifelse2.c

---
+++ ifelse2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+  int n;
+  printf("enter a number: ");
+  scanf("%d",&n);
+  if(n > 0)
+  {
+    printf("positive No");
+  }
+  else
+  {
+    printf("Non positive");
+  }
+}
